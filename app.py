38a9ee8e7219dadd2bc9abe0e38d0f06c742bf81
felixvl <digitaleservice@gmail.com>
2022-01-23 10:22:56 +0100

    csv erbij

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import pandas
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():    
     return "de flask app commit twee"
+
+
+@app.route("/twee")
+def indexTwee(): 
+    pandas.read_csv("Pokemon.csv")   
+    return "de flask app commit drie"
